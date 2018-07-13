6be2fa4355486e942535d5c8388e7613edbdeb77
Jennifer Hong <jennifer.hong13@gmail.com>
2018-07-13 04:13:53 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+  if speed == <40 || speef == >60
+    puts "safe"
+  else
+    puts "unsafe"
 end
 
 
 
 def not_safe?(speed)
-	
+  speed == <40 || speed == >60 ? "safe" : "unsafe"
 end
 	
 
